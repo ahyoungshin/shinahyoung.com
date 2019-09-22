ad1f52f9b9086ee94e9dc92cf5b5dc5c939956cb
ahyoungshin <thesyung@gmail.com>
2019-09-22 22:28:59 +0900

    2차시도

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__=='__main__':
     import os
-    app.run(port=os.environ.get('PORT', 5000))
+    app.run('0.0.0.0', port=os.environ.get('PORT', 5000))
